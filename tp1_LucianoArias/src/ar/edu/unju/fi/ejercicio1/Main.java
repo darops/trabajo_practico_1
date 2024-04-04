0e9047a14008f804ecdffe1b211da64d0e01d69b
luciano <luciano@DESKTOP-GFDPAMD>
2024-04-04 16:53:43 -0300

    punto1

--- tp1_LucianoArias/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_LucianoArias/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,8 +3,10 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Bienvenido a Programacion Visual! ");
-
+		int n1 = 8 , n2 = 5, n3 = 1, n4 = 20, n5 = 9;
+		double promedio = (n1+n2+n3+n4+n5)/5.0;
+		 System.out.println("El promedio es: "+ promedio);
+		
 	}
 
 }
