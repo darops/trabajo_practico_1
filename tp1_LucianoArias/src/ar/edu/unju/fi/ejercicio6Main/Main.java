a9e674a9d1670d98c73e17c513dfd05c6a1d9c3e
luciano <luciano@DESKTOP-GFDPAMD>
2024-04-15 01:36:11 -0300

    se agregan paquetes y clases

---
+++ tp1_LucianoArias/src/ar/edu/unju/fi/ejercicio6Main/Main.java
@@ -0,0 +1,70 @@
+package ar.edu.unju.fi.ejercicio6Main;
+
+import java.time.LocalDate;
+
+import java.time.format.DateTimeFormatter;
+
+import ar.edu.unju.fi.ejercicio6Model.Estudiante;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String Nombre,Provincia,Dni;
+		Scanner scanner = new Scanner(System.in);
+		DateTimeFormatter formato1 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		
+		
+		System.out.println("Ingrese los Datos del estudiante");
+		
+		Estudiante estudiante1 = new Estudiante();
+		System.out.println("Ingrese el nombre: ");
+		Nombre = scanner.nextLine();
+		System.out.println("Ingrese el Dni: ");
+		Dni = scanner.nextLine();
+		System.out.println("Ingrese la fecha de nacimiento: ");
+		String fecha = scanner.nextLine();
+		estudiante1.setFechaDeNac(LocalDate.parse(fecha,formato1));
+		System.out.println("Ingrese la provincia: ");
+		Provincia = scanner.nextLine();
+		
+		estudiante1.setDni(Dni);
+		estudiante1.setNombre(Nombre);
+		estudiante1.setProvincia(Provincia);
+		estudiante1.MostrarDatos();
+		
+		//declaro el segundo estudiante con el metodo parametrizado
+		System.out.println("Ingrese los Datos del estudiante");
+		
+		System.out.println("Ingrese el nombre: ");
+		Nombre = scanner.nextLine();
+		System.out.println("Ingrese el Dni: ");
+		Dni = scanner.nextLine();
+		System.out.println("Ingrese la fecha de nacimiento: ");
+		fecha = scanner.nextLine();
+		System.out.println("Ingrese la provincia: ");
+		Provincia = scanner.nextLine();
+		
+		Estudiante estudiante2 = new Estudiante(Dni,Nombre, LocalDate.parse(fecha,formato1) ,Provincia);
+		estudiante2.MostrarDatos();
+		
+		//Declaro el segundo estudiante con el metodo parametrizado sin provincia
+		System.out.println("Ingrese los Datos del estudiante");
+		
+		System.out.println("Ingrese el nombre: ");
+		Nombre = scanner.nextLine();
+		System.out.println("Ingrese el Dni: ");
+		Dni = scanner.nextLine();
+		System.out.println("Ingrese la fecha de nacimiento: ");
+		fecha = scanner.nextLine();
+		
+		Estudiante estudiante3 = new Estudiante(Dni,Nombre, LocalDate.parse(fecha,formato1));
+		estudiante3.MostrarDatos();
+		
+		scanner.close();
+		
+	}
+
+}
